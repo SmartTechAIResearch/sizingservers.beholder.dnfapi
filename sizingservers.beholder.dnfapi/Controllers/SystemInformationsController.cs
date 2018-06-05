05f37e063f1d8512b950ef2acc52a942181af0ad
Dieter Vandroemme <dietere.vandroemme@gmail.com>
2018-06-05 15:43:10 +0200

    First commit

---
+++ sizingservers.beholder.dnfapi/Controllers/SystemInformationsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace sizingservers.beholder.dnfapi.Controllers
+{
+    public class SystemInformationsController : ApiController
+    {
+        // GET: api/SystemInformations
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/SystemInformations/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/SystemInformations
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/SystemInformations/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/SystemInformations/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ sizingservers.beholder.dnfapi/Models/APIKey.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * 2018 Sizing Servers Lab
+ * University College of West-Flanders, Department GKG
+ * 
+ */
+
+using System.ComponentModel.DataAnnotations;
+
+namespace sizingservers.beholder.dnfapi.Models {
+    /// <summary>
+    /// 
+    /// </summary>
+    public class APIKey {
+        /// <summary>
+        /// When the key was issued.
+        /// </summary>
+        public long timeStampInSecondsSinceEpochUtc { get; set; }
+        /// <summary>
+        /// The e-mail adres of the user. A check should happen if this e-mail adres exists.
+        /// </summary>
+        [Key]
+        public string emailAddress { get; set; }
+        /// <summary>
+        /// No restrictions. Just a string. Should maybe be a SHA512 hash.
+        /// </summary>
+        /// <summary>
+        /// No restrictions. Just a string. Should maybe be a SHA512 hash.
+        /// </summary>
+        public string key { get; set; }
+    }
+}
